8410248adefeab2a6e265c896bfc7e034075e29b
Omel (Leon) <michelson45@gmail.com>
2025-11-30 15:44:54 +0100

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import {ErrorComponent} from './components/error/error.component';
+
+export const routes: Routes = [
+  {
+    path: '**', component: ErrorComponent
+  },
+];
